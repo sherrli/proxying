e133a06f464cd12ef73cc6f327b54732373915cf
Sherri Li <32345806+sherrli@users.noreply.github.com>
2020-06-10 16:23:17 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env/python
 
+'''
+Description: Gather the response time of a website.
+'''
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
@@ -12,12 +16,17 @@ import unittest
 
 def usage():
     msg = """
-        Usage:
-            python/python3 script.py
-
         About:
-            A script that routes headless geckodriver requests through local proxy port 8888.
-            Pre-requisite that Charles proxy configured to listen on 8888 inside com.xk72.charles.config.
+        A script that routes headless geckodriver requests through local proxy port 8888.
+        Pre-requisite that Charles proxy configured to listen on 8888 inside com.xk72.charles.config.
+        
+        
+        Usage:
+            python/python3 script.py <charles format>
+            
+        Options:
+            <charles format>    [ chls | csv | xml | json | trace | har ]
+
       """
     print(msg)
 
@@ -34,8 +43,8 @@ class Record(unittest.TestCase):
     
     browser = None
     
-    # charles_port = "http://127.0.0.1:8887" -- sometimes 127.0.0.1 traffic is ignored so use localhost below
-    charles_port = "http://localhost.charlesproxy.com:8887"
+    # charles_port = "http://127.0.0.1:8888" -- sometimes 127.0.0.1 traffic is ignored so use localhost below
+    charles_port = "http://localhost.charlesproxy.com:8888"
     charles_start = "http://control.charles/recording/start"
     charles_stop = "http://control.charles/recording/stop"
     charles_clear = "http://control.charles/session/clear"
@@ -61,26 +70,43 @@ class Record(unittest.TestCase):
     def tearDown(self):
         testName = "{}".format(self)
         save_file = testName.split(' ')[0] + ".chls"
-        save_file_converted = save_file.replace(".chls", ".chlsj")
+        save_file_converted = save_file.replace(".chls", self.session_format)
+        file_path = self.save_folder + save_file_converted
         try:
             print("\n======= Save Recording ===========")
-            os.system("curl -o " + self.save_folder + save_file_converted + " -x " + self.charles_port + " " + self.charles_download_json)
+            os.system("curl -o " + file_path + " -x " + self.charles_port + " http://control.charles/session/export-" + self.session_format)
+            if self.browser is not None:
+                print("\nClosing browser")
+                self.browser.quit()
         except Exception as e:
             print(e)
             
-    def test_allsides():   
-      try:
-          with Xvfb() as xvfb:
-              driver = webdriver.Firefox(capabilities=proxy_capability)
-              driver.implicitly_wait(30)
-              driver.delete_all_cookies()
-              self.browser = driver
-              driver.get("https://www.allsides.com/unbiased-balanced-news")
-              print(driver.current_url)
-      except Exception as e:
-          print(e)
-          raise e
+    def test_allsides():
+        print("\n Testing ..")
+        try:
+            with Xvfb() as xvfb:
+                driver = webdriver.Firefox(capabilities=proxy_capability)
+                driver.implicitly_wait(30)
+                driver.delete_all_cookies()
+                self.browser = driver
+        except Exception as e:
+            print("Error: loading firefox in virtual display")
+            print(e)
+        try:
+            assert(self.browser is not None)
+            driver.get("https://www.allsides.com/unbiased-balanced-news")
+            print(driver.current_url)
+        except Exception as e:
+            print("Error during test")
+            print(e)
     
     
 if __name__ == '__main__':
-    test()
+    import sys
+    if len(sys.argv) == 2:
+        Record.session_format = sys.argv.pop()
+        assert(Record.session_format in ["chls", "csv", "xml", "json", "trace", "har"])
+        unittest.main()
+    else:
+        print("Incorrect CLI args")
+        usage()
